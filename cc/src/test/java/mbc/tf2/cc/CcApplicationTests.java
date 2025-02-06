a6c16de944e842ecfebe0206ae2ae019be121aef
jin <dbrwls12@gmail.com>
2025-02-06 11:10:36 +0900

    다시 boot 초기 ver1.0

---
+++ cc/src/test/java/mbc/tf2/cc/CcApplicationTests.java
@@ -0,0 +1,13 @@
+package mbc.tf2.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
