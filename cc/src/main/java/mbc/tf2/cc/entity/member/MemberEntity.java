322e2d04d3d7ebd6ee1f20f67155e2ab5a44449d
312-03\mkm1022 <mkm1022@naver.com>
2025-02-19 14:49:00 +0900

    마이페이지 관련

--- cc/src/main/java/mbc/tf2/cc/entity/member/MemberEntity.java
+++ cc/src/main/java/mbc/tf2/cc/entity/member/MemberEntity.java
@@ -16,19 +16,21 @@ public class MemberEntity {
     @SequenceGenerator(name = "member_seq", sequenceName = "MEMBER_SEQ", allocationSize = 1)
     private Long id;
 
+    @Column(nullable = false)
+    private String auth = "USER";  // 기본 권한 "USER"
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
     @Column(unique = true, nullable = false)
     private String memberId;  // member_id → memberId로 수정
 
     @Column(nullable = false)
     private String memberPw;  // member_pw → memberPw로 수정
 
-    @Column(unique = true, nullable = false)
-    private String email;
 
     private String phone;
 
-    @Column(nullable = false)
-    private String auth = "USER";  // 기본 권한 "USER"
 
     // 비밀번호 암호화 저장
     public void encodePassword(BCryptPasswordEncoder encoder) {
