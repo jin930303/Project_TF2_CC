a03bd01dcec0978542e0e9a4cf2eb5e417bddbb3
shk0349 <shk0349@naver.com>
2025-02-19 15:04:02 +0900

    cctvcontroller 문장배치 수정

--- cc/src/main/java/mbc/tf2/cc/controller/cctv/CCTVController.java
+++ cc/src/main/java/mbc/tf2/cc/controller/cctv/CCTVController.java
@@ -57,58 +57,31 @@ public class CCTVController {
 
         //... 설정된 권역별 cctv 위도 및 경도 최소/최대값 설정
         if (cctv_location.equals("s_i_gg")) {
-            minX = "126.346200";
-            maxX = "127.847900";
-            minY = "36.899800";
-            maxY = "38.276600";
+            minX = "126.346200"; maxX = "127.847900"; minY = "36.899800"; maxY = "38.276600";
         }
         else if (cctv_location.equals("gw")) {
-            minX = "127.082200";
-            maxX = "129.437800";
-            minY = "37.047600";
-            maxY = "38.623800";
+            minX = "127.082200"; maxX = "129.437800"; minY = "37.047600"; maxY = "38.623800";
         }
         else if (cctv_location.equals("d_cn")) {
-            minX = "126.117400";
-            maxX = "127.633500";
-            minY = "35.983700";
-            maxY = "37.067400";
+            minX = "126.117400"; maxX = "127.633500"; minY = "35.983700"; maxY = "37.067400";
         }
         else if (cctv_location.equals("cb")) {
-            minX = "127.253200";
-            maxX = "128.648400";
-            minY = "36.01600";
-            maxY = "37.25900";
+            minX = "127.253200"; maxX = "128.648400"; minY = "36.01600"; maxY = "37.25900";
         }
         else if (cctv_location.equals("d_gb")) {
-            minX = "127.755900";
-            maxX = "129.652500";
-            minY = "35.570900";
-            maxY = "37.261300";
+            minX = "127.755900"; maxX = "129.652500"; minY = "35.570900"; maxY = "37.261300";
         }
         else if (cctv_location.equals("b_u_gn")) {
-            minX = "127.556900";
-            maxX = "129.572900";
-            minY = "34.624900";
-            maxY = "35.918600";
+            minX = "127.556900"; maxX = "129.572900"; minY = "34.624900"; maxY = "35.918600";
         }
         else if (cctv_location.equals("g_jn")) {
-            minX = "125.777400";
-            maxX = "127.809800";
-            minY = "34.109600";
-            maxY = "35.518900";
+            minX = "125.777400"; maxX = "127.809800"; minY = "34.109600"; maxY = "35.518900";
         }
         else if (cctv_location.equals("jb")) {
-            minX = "126.370300";
-            maxX = "127.913900";
-            minY = "35.283400";
-            maxY = "36.168600";
+            minX = "126.370300"; maxX = "127.913900"; minY = "35.283400"; maxY = "36.168600";
         }
         else if (cctv_location.equals("jj")) {
-            minX = "126.096200";
-            maxX = "127.005100";
-            minY = "33.175100";
-            maxY = "33.642000";
+            minX = "126.096200"; maxX = "127.005100"; minY = "33.175100"; maxY = "33.642000";
         }
 
         //... API URL 설정
